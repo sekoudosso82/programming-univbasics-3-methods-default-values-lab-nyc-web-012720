a8d0aafded70a5ba2484334509b7b2feab0ed335
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 00:05:16 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,6 @@
 
 def meal_choice(veg1, veg2, protein ="meat")
   puts "What a nutritious meal!"
-  return "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "#{protein}, #{veg1}, #{veg2}"
 end 
